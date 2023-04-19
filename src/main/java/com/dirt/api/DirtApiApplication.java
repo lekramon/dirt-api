127694360f6202ab98fe1d59a0ae21bb2cde1111
lekramon <ramon.pinheiro20@gmail.com>
2023-04-19 12:39:52 -0300

    build: created project with necessary Spring dependencies and configured .gitignore.

---
+++ src/main/java/com/dirt/api/DirtApiApplication.java
@@ -0,0 +1,13 @@
+package com.dirt.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirtApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirtApiApplication.class, args);
+	}
+
+}
