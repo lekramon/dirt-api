95a859607e989d5597a7d325098282eb7b05b9f8
lekramon <ramon.pinheiro20@gmail.com>
2023-04-20 12:09:06 -0300

    feat: moved HelloWorld from DirtApiApplication to a new controller
    RG-6397

--- src/main/java/com/dirt/api/DirtApiApplication.java
+++ src/main/java/com/dirt/api/DirtApiApplication.java
@@ -2,20 +2,11 @@ package com.dirt.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class DirtApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DirtApiApplication.class, args);
-	}
-
-	@GetMapping("/")
-	public String index() {
-		return "Hello World!";
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DirtApiApplication.class, args);
+    }
 }
