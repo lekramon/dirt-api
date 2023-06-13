b89a4cabd0f3429346969116d128284e5bd5079d
lekramon <ramon.pinheiro20@gmail.com>
2023-06-13 18:26:36 -0300

    feat: Instantiating entity within the factory
    RG-6424

--- src/main/java/com/dirt/api/usecase/factory/TransactionFactory.java
+++ src/main/java/com/dirt/api/usecase/factory/TransactionFactory.java
@@ -1,5 +1,7 @@
 package com.dirt.api.usecase.factory;
 
+import com.dirt.api.adapter.dto.request.CaptureMethodRequest;
+import com.dirt.api.adapter.dto.request.OtherAccountRequest;
 import com.dirt.api.adapter.dto.request.TransactionRequest;
 import com.dirt.api.domain.entity.TransactionEntity;
 import com.dirt.api.domain.enums.StatusEnum;
@@ -13,8 +15,21 @@ public class TransactionFactory {
     public TransactionEntity registerTransaction(TransactionRequest transactionRequest) {
         LocalDateTime dateTime = LocalDateTime.now(ZoneId.of("America/Sao_Paulo"));
         TransactionEntity transactionEntity = new TransactionEntity();
+        CaptureMethodRequest captureMethodRequest = transactionRequest.getCaptureMethodRequest();
+        OtherAccountRequest otherAccountRequest = transactionRequest.getOtherAccountRequest();
+        transactionEntity.setTransactionIp(transactionRequest.getIp());
+        transactionEntity.setTransactionAmount(transactionRequest.getAmount());
+        transactionEntity.setTransactionTax(transactionRequest.getTax());
+        transactionEntity.setTransactionAccount(transactionRequest.getAccountId());
+        transactionEntity.setTransactionCaptureMethod(captureMethodRequest.getId());
+        transactionEntity.setCaptureMethod(captureMethodRequest.getType());
+        transactionEntity.setTransactionType(transactionRequest.getTransactionType());
+        transactionEntity.setOperation(transactionRequest.getOperation());
         transactionEntity.setTransactionDat(Timestamp.valueOf(dateTime));
         transactionEntity.setStatus(StatusEnum.PENDING);
+        transactionEntity.setTransactionOtherAccount(otherAccountRequest.getNumber());
+        transactionEntity.setTransactionOtherAccountAgency(otherAccountRequest.getAgency());
+        transactionEntity.setTransactionOtherAccountBank(otherAccountRequest.getBankCode());
         return transactionEntity;
     }
 }
