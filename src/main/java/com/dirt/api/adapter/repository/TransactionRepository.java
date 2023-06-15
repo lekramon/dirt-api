276e55f0ae67f37a71b8322f9cdb1a9df53fc209
lekramon <ramon.pinheiro20@gmail.com>
2023-06-15 12:38:23 -0300

    refactor: change type Id to Long
    RG-6424

--- src/main/java/com/dirt/api/adapter/repository/TransactionRepository.java
+++ src/main/java/com/dirt/api/adapter/repository/TransactionRepository.java
@@ -3,7 +3,5 @@ package com.dirt.api.adapter.repository;
 import com.dirt.api.domain.entity.TransactionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.persistence.Id;
-
-public interface TransactionRepository extends JpaRepository<TransactionEntity, Id> {
+public interface TransactionRepository extends JpaRepository<TransactionEntity, Long> {
 }
