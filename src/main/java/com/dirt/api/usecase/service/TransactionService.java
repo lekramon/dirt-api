c80bdaa9c7df9a9e4d9e7350f84393375be8a08d
lekramon <ramon.pinheiro20@gmail.com>
2023-06-15 12:32:25 -0300

    feat: adding private method getAccountEntityById to check accountRepository if account exists
    RG-6424

--- src/main/java/com/dirt/api/usecase/service/TransactionService.java
+++ src/main/java/com/dirt/api/usecase/service/TransactionService.java
@@ -1,22 +1,38 @@
 package com.dirt.api.usecase.service;
 
 import com.dirt.api.adapter.dto.request.TransactionRequest;
+import com.dirt.api.adapter.repository.AccountRepository;
 import com.dirt.api.adapter.repository.TransactionRepository;
+import com.dirt.api.domain.entity.AccountEntity;
 import com.dirt.api.domain.entity.TransactionEntity;
+import com.dirt.api.domain.exception.AccountNotExistException;
 import com.dirt.api.usecase.factory.TransactionFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class TransactionService {
 
     final TransactionRepository transactionRepository;
+    final AccountRepository accountRepository;
 
-    public TransactionService(TransactionRepository transactionRepository) {
+    public TransactionService(TransactionRepository transactionRepository, AccountRepository accountRepository) {
         this.transactionRepository = transactionRepository;
+        this.accountRepository = accountRepository;
     }
 
     public TransactionEntity register(TransactionRequest transactionRequest) {
+        AccountEntity accountEntity = getAccountEntityById(transactionRequest.getAccountId());
         TransactionFactory transactionFactory = new TransactionFactory();
-        return transactionRepository.save(transactionFactory.registerTransaction(transactionRequest));
+        return transactionRepository.save(transactionFactory.registerTransaction(transactionRequest, accountEntity));
+    }
+
+    private AccountEntity getAccountEntityById(long id) {
+        final Optional<AccountEntity> accountEntity = accountRepository.findById(id);
+        if (accountEntity.isEmpty()) {
+            throw new AccountNotExistException("This account id does not exist");
+        }
+        return accountEntity.get();
     }
 }
