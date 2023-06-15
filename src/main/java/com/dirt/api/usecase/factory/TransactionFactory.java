9cd4b1fd9bd9ce587f32ad4a0d516ee000a299e5
lekramon <ramon.pinheiro20@gmail.com>
2023-06-15 12:39:56 -0300

    feat: adding parameter AccountEntity on registerTransaction constructor
    RG-6424

--- src/main/java/com/dirt/api/usecase/factory/TransactionFactory.java
+++ src/main/java/com/dirt/api/usecase/factory/TransactionFactory.java
@@ -3,6 +3,7 @@ package com.dirt.api.usecase.factory;
 import com.dirt.api.adapter.dto.request.CaptureMethodRequest;
 import com.dirt.api.adapter.dto.request.OtherAccountRequest;
 import com.dirt.api.adapter.dto.request.TransactionRequest;
+import com.dirt.api.domain.entity.AccountEntity;
 import com.dirt.api.domain.entity.TransactionEntity;
 import com.dirt.api.domain.enums.StatusEnum;
 
@@ -12,7 +13,7 @@ import java.time.ZoneId;
 
 public class TransactionFactory {
 
-    public TransactionEntity registerTransaction(TransactionRequest transactionRequest) {
+    public TransactionEntity registerTransaction(TransactionRequest transactionRequest, AccountEntity accountEntity) {
         Timestamp timestamp = new Timestamp(System.currentTimeMillis());
         TransactionEntity transactionEntity = new TransactionEntity();
         CaptureMethodRequest captureMethodRequest = transactionRequest.getCaptureMethodRequest();
@@ -20,7 +21,7 @@ public class TransactionFactory {
         transactionEntity.setTransactionIp(transactionRequest.getIp());
         transactionEntity.setTransactionAmount(transactionRequest.getAmount());
         transactionEntity.setTransactionTax(transactionRequest.getTax());
-        transactionEntity.setTransactionAccount(transactionRequest.getAccountId());
+        transactionEntity.setTransactionAccount(accountEntity);
         transactionEntity.setTransactionDes(transactionRequest.getDescription());
         transactionEntity.setTransactionCaptureMethod(captureMethodRequest.getId());
         transactionEntity.setCaptureMethod(captureMethodRequest.getType());
