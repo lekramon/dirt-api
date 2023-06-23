4fe80bcceccc51bb3f7f15223df5900167eb0ed2
lekramon <ramon.pinheiro20@gmail.com>
2023-06-23 12:25:08 -0300

    test: adding unit tests to CaptureMethodEnumConverter
    RG-6424

---
+++ src/test/java/com/dirt/api/domain/enums/converter/CaptureMethodEnumConverterTest.java
@@ -0,0 +1,45 @@
+package com.dirt.api.domain.enums.converter;
+
+import com.dirt.api.domain.enums.CaptureMethodEnum;
+import com.dirt.api.domain.exception.EnumNotExistException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class CaptureMethodEnumConverterTest {
+
+    private final CaptureMethodEnumConverter captureMethodEnumConverter = new CaptureMethodEnumConverter();
+
+    @Test
+    public void shouldConvertToDatabaseColumn() {
+        CaptureMethodEnum captureMethodEnum = CaptureMethodEnum.WEB;
+        Integer databaseColumn = captureMethodEnumConverter.convertToDatabaseColumn(captureMethodEnum);
+        Assertions.assertEquals(captureMethodEnum.getCaptureMethodType(), databaseColumn);
+    }
+
+    @Test
+    public void shouldReturnNullToDatabaseColumn() {
+        Integer nullDatabaseColumn = captureMethodEnumConverter.convertToDatabaseColumn(null);
+        Assertions.assertNull(nullDatabaseColumn);
+    }
+
+    @Test
+    public void shouldConvertToEntityAttribute() {
+        int captureMethodValue = 1;
+        CaptureMethodEnum captureMethodEnum = captureMethodEnumConverter.convertToEntityAttribute(captureMethodValue);
+        Assertions.assertEquals(CaptureMethodEnum.fromCode(captureMethodValue), captureMethodEnum);
+    }
+
+    @Test
+    public void shouldReturnNullToEntityAttribute() {
+        CaptureMethodEnum captureMethodEnum = captureMethodEnumConverter.convertToEntityAttribute(null);
+        Assertions.assertNull(captureMethodEnum);
+    }
+
+    @Test
+    public void shouldThrowEnumNotExistExceptionToCaptureMethod() {
+        int captureMethodValue = 6;
+        Assertions.assertThrows(EnumNotExistException.class, () -> {
+            CaptureMethodEnum captureMethodEnum = captureMethodEnumConverter.convertToEntityAttribute(captureMethodValue);
+        });
+    }
+}
