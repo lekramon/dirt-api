07653d8ecf66537506b3fad40707e532964817d4
lekramon <ramon.pinheiro20@gmail.com>
2023-06-23 12:26:47 -0300

    test: adding unit tests to OperationEnumConverter
    RG-6424

---
+++ src/test/java/com/dirt/api/domain/enums/converter/OperationEnumConverterTest.java
@@ -0,0 +1,45 @@
+package com.dirt.api.domain.enums.converter;
+
+import com.dirt.api.domain.enums.OperationEnum;
+import com.dirt.api.domain.exception.EnumNotExistException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class OperationEnumConverterTest {
+
+    private final OperationEnumConverter operationEnumConverter = new OperationEnumConverter();
+
+    @Test
+    public void shouldConvertToDatabaseColumn() {
+        OperationEnum operationEnum = OperationEnum.DEBIT;
+        Integer databaseColumn = operationEnumConverter.convertToDatabaseColumn(operationEnum);
+        Assertions.assertEquals(operationEnum.getOperation(), databaseColumn);
+    }
+
+    @Test
+    public void shouldReturnNullToDatabaseColumn() {
+        Integer nullDatabaseColumn = operationEnumConverter.convertToDatabaseColumn(null);
+        Assertions.assertNull(nullDatabaseColumn);
+    }
+
+    @Test
+    public void shouldConvertToEntityAttribute() {
+        int operationEnumValue = 1;
+        OperationEnum operationEnum = operationEnumConverter.convertToEntityAttribute(operationEnumValue);
+        Assertions.assertEquals(OperationEnum.fromCode(operationEnumValue), operationEnum);
+    }
+
+    @Test
+    public void shouldReturnNullToEntityAttribute() {
+        OperationEnum operationEnum = operationEnumConverter.convertToEntityAttribute(null);
+        Assertions.assertNull(operationEnum);
+    }
+
+    @Test
+    public void shouldThrowEnumNotExistExceptionToOperation() {
+        int operationValue = 6;
+        Assertions.assertThrows(EnumNotExistException.class, () -> {
+            OperationEnum operationEnum = operationEnumConverter.convertToEntityAttribute(operationValue);
+        });
+    }
+}
