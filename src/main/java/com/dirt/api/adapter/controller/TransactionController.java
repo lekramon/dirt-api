d8861698dd4f99f13916ff48c546b97a3b12df34
lekramon <ramon.pinheiro20@gmail.com>
2023-06-27 15:53:29 -0300

    feat: adding @Valid to register controller
    RG-6424

--- src/main/java/com/dirt/api/adapter/controller/TransactionController.java
+++ src/main/java/com/dirt/api/adapter/controller/TransactionController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/transaction")
 public class TransactionController {
@@ -22,7 +24,7 @@ public class TransactionController {
     }
 
     @PostMapping
-    public ResponseEntity<TransactionEntity> registerTransaction(@RequestBody TransactionRequest transactionRequest) {
+    public ResponseEntity<TransactionEntity> registerTransaction(@RequestBody @Valid TransactionRequest transactionRequest) {
         return ResponseEntity.status(HttpStatus.CREATED).body(transactionService.register(transactionRequest));
     }
 }
