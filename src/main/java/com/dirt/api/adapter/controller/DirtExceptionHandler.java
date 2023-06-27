eb31bf81c88fdbd1a88e8166db750920ffd5bb59
lekramon <ramon.pinheiro20@gmail.com>
2023-06-27 12:15:38 -0300

    fix: adding AccountNotExistException on @ExceptionHandler parameter
    RG-6424

--- src/main/java/com/dirt/api/adapter/controller/DirtExceptionHandler.java
+++ src/main/java/com/dirt/api/adapter/controller/DirtExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.dirt.api.adapter.controller;
 
 import com.dirt.api.adapter.dto.response.ErrorResponse;
+import com.dirt.api.domain.exception.AccountNotExistException;
 import com.dirt.api.domain.exception.EnumNotExistException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class DirtExceptionHandler {
 
-    @ExceptionHandler(EnumNotExistException.class)
+    @ExceptionHandler({EnumNotExistException.class, AccountNotExistException.class})
     public ResponseEntity<ErrorResponse> handleBadRequestException(RuntimeException exception) {
 
         return ResponseEntity.badRequest().body(new ErrorResponse(HttpStatus.BAD_REQUEST.value(), exception.getMessage()));
