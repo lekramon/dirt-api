8fc7a25a64df5a691108e9f1ee1094f465d6dd1e
lekramon <ramon.pinheiro20@gmail.com>
2023-07-11 17:58:44 -0300

    test: adding webEnvironment and set to Random Port
    RG-6424

--- src/test/java/com/dirt/api/cucumber/config/CucumberSpringConfig.java
+++ src/test/java/com/dirt/api/cucumber/config/CucumberSpringConfig.java
@@ -6,6 +6,6 @@ import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @CucumberContextConfiguration
-@SpringBootTest(classes = DirtApiApplication.class)
+@SpringBootTest(classes = DirtApiApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CucumberSpringConfig {
 }
