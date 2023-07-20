96557656d303f5dd054a27cd8e3c681e02599f45
lekramon <ramon.pinheiro20@gmail.com>
2023-07-20 17:45:09 -0300

    feat: adding updateTransactionStatus, validateStatus and updateTransactionStatusById
    RG-6808

--- src/main/java/com/dirt/api/usecase/service/TransactionService.java
+++ src/main/java/com/dirt/api/usecase/service/TransactionService.java
@@ -1,11 +1,15 @@
 package com.dirt.api.usecase.service;
 
 import com.dirt.api.adapter.dto.request.TransactionRequest;
+import com.dirt.api.adapter.dto.request.UpdateStatusRequest;
 import com.dirt.api.adapter.repository.AccountRepository;
 import com.dirt.api.adapter.repository.TransactionRepository;
 import com.dirt.api.domain.entity.AccountEntity;
 import com.dirt.api.domain.entity.TransactionEntity;
+import com.dirt.api.domain.enums.StatusEnum;
 import com.dirt.api.domain.exception.AccountNotExistException;
+import com.dirt.api.domain.exception.StatusValidateException;
+import com.dirt.api.domain.exception.TransactionNotExistException;
 import com.dirt.api.usecase.factory.TransactionFactory;
 import org.springframework.stereotype.Service;
 
@@ -20,13 +24,42 @@ public class TransactionService {
         this.accountRepository = accountRepository;
     }
 
-    public TransactionEntity register(TransactionRequest transactionRequest) {
+    public TransactionEntity registerTransaction(TransactionRequest transactionRequest) {
         final AccountEntity accountEntity = getAccountEntityById(transactionRequest.getAccountId());
         return transactionRepository.save(TransactionFactory.createTransaction(transactionRequest, accountEntity));
+    }
+
+    public TransactionEntity updateTransactionStatusById(long id, UpdateStatusRequest updateStatusRequest) {
+        final TransactionEntity transactionEntity = getTransactionEntityById(id);
+        validateStatusTransactionFromStateMachine(transactionEntity.getStatus(), StatusEnum.fromValue(updateStatusRequest.getStatus()));
+        return transactionRepository.save(updateTransactionStatusById(updateStatusRequest, transactionEntity));
     }
 
     private AccountEntity getAccountEntityById(long id) {
         return accountRepository.findById(id)
                 .orElseThrow(() -> new AccountNotExistException("This account id: " + id + " doesn't exist"));
+    }
+
+    private TransactionEntity getTransactionEntityById(long id) {
+        return transactionRepository.findById(id)
+                .orElseThrow(() -> new TransactionNotExistException("This transaction id: " + id + " doesn't exist"));
+    }
+
+    private TransactionEntity updateTransactionStatusById(UpdateStatusRequest updateStatusRequest, TransactionEntity transactionEntity) {
+
+        transactionEntity.setStatus(StatusEnum.fromValue(updateStatusRequest.getStatus()));
+
+        return transactionEntity;
+    }
+
+    private void validateStatusTransactionFromStateMachine(StatusEnum currentStatus, StatusEnum newStatus) {
+
+        if ((currentStatus == StatusEnum.SUCCESS || currentStatus == StatusEnum.CANCELED) && (newStatus == StatusEnum.CANCELED || newStatus == StatusEnum.PENDING || newStatus == StatusEnum.SUCCESS)) {
+            throw new StatusValidateException("Cannot update status from " + currentStatus + " to " + newStatus);
+        }
+
+        if (currentStatus == newStatus) {
+            throw new StatusValidateException("Cannot update status from " + currentStatus + " to " + newStatus);
+        }
     }
 }
