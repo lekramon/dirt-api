09a471e59eb4730347d3a7ff3c4686675fb8c5a7
lekramon <ramon.pinheiro20@gmail.com>
2023-07-20 17:47:41 -0300

    feat: adding put endpoint
    RG-6808

--- src/main/java/com/dirt/api/adapter/controller/TransactionController.java
+++ src/main/java/com/dirt/api/adapter/controller/TransactionController.java
@@ -2,15 +2,13 @@ package com.dirt.api.adapter.controller;
 
 
 import com.dirt.api.adapter.dto.request.TransactionRequest;
+import com.dirt.api.adapter.dto.request.UpdateStatusRequest;
 import com.dirt.api.adapter.dto.response.TransactionResponse;
 import com.dirt.api.usecase.factory.TransactionResponseFactory;
 import com.dirt.api.usecase.service.TransactionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -26,7 +24,13 @@ public class TransactionController {
 
     @PostMapping
     public ResponseEntity<TransactionResponse> registerTransaction(@Valid @RequestBody TransactionRequest transactionRequest) {
-        final TransactionResponse transactionResponse = TransactionResponseFactory.createTransactionResponse(transactionService.register(transactionRequest));
+        final TransactionResponse transactionResponse = TransactionResponseFactory.createTransactionResponse(transactionService.registerTransaction(transactionRequest));
         return ResponseEntity.status(HttpStatus.CREATED).body(transactionResponse);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TransactionResponse> updateTransactionStatus(@PathVariable long id, @Valid @RequestBody UpdateStatusRequest updateStatusRequest) {
+        final TransactionResponse transactionResponse = TransactionResponseFactory.createTransactionResponse(transactionService.updateTransactionStatusById(id, updateStatusRequest));
+        return ResponseEntity.status(HttpStatus.OK).body(transactionResponse);
     }
 }
