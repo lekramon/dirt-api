740b1ed607c3870a3fe5262ef529fe8ff7326d38
lekramon <ramon.pinheiro20@gmail.com>
2023-07-26 12:43:09 -0300

    refactor: removing wildcard imports
    RG-6808

--- src/main/java/com/dirt/api/domain/entity/TransactionEntity.java
+++ src/main/java/com/dirt/api/domain/entity/TransactionEntity.java
@@ -5,7 +5,14 @@ import com.dirt.api.domain.enums.OperationEnum;
 import com.dirt.api.domain.enums.StatusEnum;
 import com.dirt.api.domain.enums.TransactionTypeEnum;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
 
