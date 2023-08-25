7588e5b20fdce155b5002a610b0726c530cd8e44
lekramon <ramon.pinheiro20@gmail.com>
2023-08-25 11:49:35 -0300

    feat: adding InvalidPageException
    RG-6809

--- src/main/java/com/dirt/api/adapter/controller/DirtExceptionHandler.java
+++ src/main/java/com/dirt/api/adapter/controller/DirtExceptionHandler.java
@@ -1,10 +1,7 @@
 package com.dirt.api.adapter.controller;
 
 import com.dirt.api.adapter.dto.response.ErrorResponse;
-import com.dirt.api.domain.exception.AccountNotExistException;
-import com.dirt.api.domain.exception.EnumNotExistException;
-import com.dirt.api.domain.exception.StatusValidateException;
-import com.dirt.api.domain.exception.TransactionNotExistException;
+import com.dirt.api.domain.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.ObjectError;
@@ -18,7 +15,7 @@ import java.util.List;
 @RestControllerAdvice
 public class DirtExceptionHandler {
 
-    @ExceptionHandler({EnumNotExistException.class, AccountNotExistException.class})
+    @ExceptionHandler({EnumNotExistException.class, AccountNotExistException.class, InvalidPageException.class})
     public ResponseEntity<ErrorResponse> handleBadRequestException(RuntimeException exception) {
         return ResponseEntity.badRequest().body(new ErrorResponse(HttpStatus.BAD_REQUEST.value(), exception.getMessage()));
     }
---
+++ src/main/java/com/dirt/api/domain/exception/InvalidPageException.java
@@ -0,0 +1,8 @@
+package com.dirt.api.domain.exception;
+
+public class InvalidPageException extends RuntimeException {
+
+    public InvalidPageException(String message) {
+        super(message);
+    }
+}
