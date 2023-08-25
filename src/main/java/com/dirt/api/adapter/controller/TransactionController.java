9ec498258d7ae5154702bc685e0ab0f28a45691b
lekramon <ramon.pinheiro20@gmail.com>
2023-08-25 11:57:22 -0300

    feat: adding endpoint to get transaction with filter and paginable
    RG-6809

--- src/main/java/com/dirt/api/adapter/controller/TransactionController.java
+++ src/main/java/com/dirt/api/adapter/controller/TransactionController.java
@@ -2,20 +2,15 @@ package com.dirt.api.adapter.controller;
 
 
 import com.dirt.api.adapter.dto.request.TransactionRequest;
+import com.dirt.api.adapter.dto.request.TransactionSearch;
 import com.dirt.api.adapter.dto.request.UpdateStatusRequest;
+import com.dirt.api.adapter.dto.response.TransactionListResponse;
 import com.dirt.api.adapter.dto.response.TransactionResponse;
 import com.dirt.api.usecase.factory.TransactionResponseFactory;
 import com.dirt.api.usecase.service.TransactionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -51,5 +46,15 @@ public class TransactionController {
     public ResponseEntity<TransactionResponse> getTransactionById(@PathVariable("id") long transactionId) {
         final TransactionResponse transactionResponse = TransactionResponseFactory.createTransactionResponse(transactionService.getTransactionById(transactionId));
         return ResponseEntity.status(HttpStatus.OK).body(transactionResponse);
+    }
+
+    @GetMapping
+    public ResponseEntity<TransactionListResponse> getTransactionsList(@RequestParam(name = "page", defaultValue = "0") int page,
+                                                                       @RequestParam(name = "captureMethodType", required = false) String captureMethodType,
+                                                                       @RequestParam(name = "transactionType", required = false) String transactionType) {
+        final TransactionSearch transactionSearch = new TransactionSearch(captureMethodType, transactionType);
+        final TransactionListResponse transactionListResponse = TransactionResponseFactory.createListTransactionResponse(transactionService.getTransactionList(page, transactionSearch));
+
+        return ResponseEntity.status(HttpStatus.OK).body(transactionListResponse);
     }
 }
--- src/main/java/com/dirt/api/usecase/service/TransactionService.java
+++ src/main/java/com/dirt/api/usecase/service/TransactionService.java
@@ -1,6 +1,7 @@
 package com.dirt.api.usecase.service;
 
 import com.dirt.api.adapter.dto.request.TransactionRequest;
+import com.dirt.api.adapter.dto.request.TransactionSearch;
 import com.dirt.api.adapter.dto.request.UpdateStatusRequest;
 import com.dirt.api.adapter.repository.AccountRepository;
 import com.dirt.api.adapter.repository.TransactionRepository;
@@ -8,9 +9,17 @@ import com.dirt.api.domain.entity.AccountEntity;
 import com.dirt.api.domain.entity.TransactionEntity;
 import com.dirt.api.domain.enums.StatusEnum;
 import com.dirt.api.domain.exception.AccountNotExistException;
+import com.dirt.api.domain.exception.InvalidPageException;
 import com.dirt.api.domain.exception.StatusValidateException;
 import com.dirt.api.domain.exception.TransactionNotExistException;
 import com.dirt.api.usecase.factory.TransactionFactory;
+import com.dirt.api.usecase.factory.TransactionSearchPredicateFactory;
+import com.dirt.api.usecase.validator.TransactionSearchValidator;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -44,6 +53,12 @@ public class TransactionService {
         return getTransactionEntityById(transactionId);
     }
 
+    public Page<TransactionEntity> getTransactionList(int page, TransactionSearch transactionSearch) {
+        final Pageable pageable = PageRequest.of(validatePage(page), 10, Sort.Direction.DESC, "transactionId");
+        final BooleanExpression predicate = TransactionSearchPredicateFactory.createPredicate(TransactionSearchValidator.validate(transactionSearch));
+        return transactionRepository.findAll(predicate, pageable);
+    }
+
     private AccountEntity getAccountEntityById(long accountId) {
         return accountRepository.findById(accountId)
                 .orElseThrow(() -> new AccountNotExistException("This account id: " + accountId + " doesn't exist"));
@@ -64,5 +79,12 @@ public class TransactionService {
         if (currentStatus == StatusEnum.SUCCESS || currentStatus == StatusEnum.CANCELED) {
             throw new StatusValidateException("Cannot update " + currentStatus + " transaction");
         }
+    }
+
+    private int validatePage(int page) {
+        if (page < 0) {
+            throw new InvalidPageException("Page index must not be less than zero");
+        }
+        return page;
     }
 }
