ba34c6e0960e724836d54a2a9a4cd7a1b588fa1b
lekramon <ramon.pinheiro20@gmail.com>
2023-08-31 16:15:17 -0300

    feat: adding GetTransactionSteps
    RG-6809

--- src/test/java/com/dirt/api/cucumber/GetTransactionSteps.java
+++ src/test/java/com/dirt/api/cucumber/GetTransactionSteps.java
@@ -5,7 +5,6 @@ import com.dirt.api.adapter.dto.response.TransactionResponse;
 import com.dirt.api.adapter.repository.TransactionRepository;
 import com.dirt.api.domain.entity.TransactionEntity;
 import io.cucumber.java.pt.Dado;
-import io.cucumber.java.pt.E;
 import io.cucumber.java.pt.Então;
 import io.cucumber.java.pt.Quando;
 import io.restassured.RestAssured;
@@ -14,6 +13,7 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.HashMap;
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
@@ -26,8 +26,9 @@ public class GetTransactionSteps {
     private static final String SERVER = "http://localhost:";
     private final TransactionRepository transactionRepository;
     private final int serverPort;
-
-    private ResponseEntity<TransactionListResponse> responseEntity;
+    private ResponseEntity<Object> transactionListResponseEntity;
+    private ResponseEntity<Object> transactionResponseEntity;
+    private final HashMap<String, String> params = new HashMap<String, String>();
 
     public GetTransactionSteps(TransactionRepository transactionRepository, @LocalServerPort int serverPort) {
         this.transactionRepository = transactionRepository;
@@ -35,39 +36,85 @@ public class GetTransactionSteps {
     }
 
     @Dado("que existam as seguintes transações no banco de dados")
-    public void queExistamAsSeguintesTransaçõesNoBancoDeDados(List<TransactionEntity> transactionEntity) {
+    public void shouldExistTransactionInTheDatabase(List<TransactionEntity> transactionEntity) {
         transactionRepository.saveAll(transactionEntity);
-        List<TransactionEntity> transactionEntityOptional = transactionRepository.findAll();
     }
 
-    @Quando("uma requisição de consulta de transações seja realizada")
-    public void umaRequisiçãoDeConsultaDeTransaçõesSejaRealizada() {
+    @Quando("uma requisição de consulta de transações for realizada")
+    public void callGetTransactionWithDefaultPage() {
         getTransactionList(DEFAULT_PAGE);
     }
 
-    @Então("o serviço de listagem deve retornar o status code {int} - {string}")
-    public void oServiçoDeListagemDeveRetornarOStatusCode(int expectedStatusCode, String expectedDesCode) {
-        assertEquals(expectedStatusCode, responseEntity.getStatusCodeValue());
-        assertEquals(expectedDesCode, HttpStatus.valueOf(responseEntity.getStatusCodeValue()).getReasonPhrase());
+    @Quando("uma requisição de consulta de transações com a página {int} for realizada")
+    public void callGetTransactionWithPage(int page) {
+        getTransactionList(page);
     }
 
-    @E("as seguintes transações devem ser retornadas para a página {int}")
-    public void asSeguintesTransaçõesDevemSerRetornadasParaAPágina(int page, List<TransactionResponse> transactionResponse) {
-        getTransactionList(page);
+    @Quando("uma requisição de consulta de transações com transactionId {int} seja realizada")
+    public void callGetTransactionById(int transactionId) {
+        getTransactionById(transactionId);
+    }
 
-        final TransactionListResponse actualTransactionListResponse = responseEntity.getBody();
+    @Quando("uma requisição de consulta de transações com os parâmetros {string} valor {string} seja realizada")
+    public void givenRequestParameter(String key, String value) {
+        params.put(key, value);
+        getTransactionList(DEFAULT_PAGE);
+    }
+
+    @Quando("uma requisição de consulta de transações com os parâmetros {string} valor {string} e {string} valor {string} seja realizada")
+    public void givenRequestParameter(String firstKey, String firstValue, String secondKey, String secondValue) {
+        givenRequestParameter(firstKey, firstValue);
+        givenRequestParameter(secondKey, secondValue);
+    }
+
+    @Então("o serviço de listagem deve retornar o status code {int} - {string}")
+    public void getServiceShouldReturnStatusCode(int expectedStatusCode, String expectedDesCode) {
+        assertEquals(expectedStatusCode, transactionListResponseEntity.getStatusCodeValue());
+        assertEquals(expectedDesCode, HttpStatus.valueOf(transactionListResponseEntity.getStatusCodeValue()).getReasonPhrase());
+    }
+
+    @Então("o serviço de listagem pelo id deve retornar o status code {int} - {string}")
+    public void getServiceByIdShouldReturnStatusCode(int expectedStatusCode, String expectedDesCode) {
+        assertEquals(expectedStatusCode, transactionResponseEntity.getStatusCodeValue());
+        assertEquals(expectedDesCode, HttpStatus.valueOf(transactionResponseEntity.getStatusCodeValue()).getReasonPhrase());
+    }
+
+    @Então("as seguintes transações devem ser retornadas")
+    public void thenReturnTransactions(List<TransactionResponse> transactionResponse) {
+        final TransactionListResponse actualTransactionListResponse = (TransactionListResponse) transactionListResponseEntity.getBody();
         assert actualTransactionListResponse != null;
 
-        final TransactionListResponse expectedTransactionListResponse = new TransactionListResponse(transactionResponse.size(), actualTransactionListResponse.getTotalSize(), page, actualTransactionListResponse.getTotalPages(), transactionResponse);
+        final TransactionListResponse expectedTransactionListResponse = new TransactionListResponse(transactionResponse.size(), actualTransactionListResponse.getTotalSize(), actualTransactionListResponse.getPage(), actualTransactionListResponse.getTotalPages(), transactionResponse);
 
         assertThat(actualTransactionListResponse).usingRecursiveComparison().isEqualTo(expectedTransactionListResponse);
     }
 
-    private ResponseEntity<TransactionListResponse> createResponseEntityFromResponse(Response response) {
+    @Então("a seguinte transação deve ser retornada")
+    public void thenReturnTransaction(TransactionResponse transactionResponse) {
+        final TransactionResponse actualTransactionResponse = (TransactionResponse) transactionResponseEntity.getBody();
+
+        assertThat(actualTransactionResponse).usingRecursiveComparison().isEqualTo(transactionResponse);
+    }
+
+    @Então("o conteúdo retornado deve ser vazio")
+    public void thenReturnNullContent() {
+        final TransactionListResponse actualTransactionListResponse = (TransactionListResponse) transactionListResponseEntity.getBody();
+        assert actualTransactionListResponse != null;
+        assertEquals(0, actualTransactionListResponse.getTotalSize());
+    }
+
+    private ResponseEntity<Object> createListResponseEntityFromResponse(Response response) {
         if (response.getStatusCode() == HttpStatus.OK.value()) {
             return new ResponseEntity<>(response.as(TransactionListResponse.class), HttpStatus.valueOf(response.getStatusCode()));
         }
-        return null;
+        return new ResponseEntity<>(response, HttpStatus.valueOf(response.getStatusCode()));
+    }
+
+    private ResponseEntity<Object> createResponseEntityFromResponse(Response response) {
+        if (response.getStatusCode() == HttpStatus.OK.value()) {
+            return new ResponseEntity<>(response.as(TransactionResponse.class), HttpStatus.valueOf(response.getStatusCode()));
+        }
+        return new ResponseEntity<>(response, HttpStatus.valueOf(response.getStatusCode()));
     }
 
     private void getTransactionList(int page) {
@@ -75,13 +122,26 @@ public class GetTransactionSteps {
 
         final Response response = given()
                 .header("Content-type", "application/json")
-                .queryParam("page", page)
                 .when()
+                .queryParam("page", page)
+                .queryParams(params)
                 .get("/transactions")
                 .then()
                 .extract().response();
 
-        this.responseEntity = createResponseEntityFromResponse(response);
+        this.transactionListResponseEntity = createListResponseEntityFromResponse(response);
     }
 
+    private void getTransactionById(int transactionId) {
+        RestAssured.baseURI = SERVER + serverPort;
+
+        final Response response = given()
+                .header("Content-type", "application/json")
+                .when()
+                .get("/transactions/" + transactionId)
+                .then()
+                .extract().response();
+
+        this.transactionResponseEntity = createResponseEntityFromResponse(response);
+    }
 }
